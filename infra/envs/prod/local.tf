cc990fe75cc32d969c6c3ed4eafa0987e061f4f0
prabhat <Rupali@com>
2026-08-18 22:30:31 +0530

    adding prod changes

---
+++ infra/envs/prod/local.tf
@@ -0,0 +1,12 @@
+locals {
+
+  project_id             = "project-39eb557f-9be4-42ee-b0c"
+  environment            = "prod"
+  region                 = "us-central1"
+  gke_cluster_name       = "assignement"
+  deployment_sa_name     = "my-sa-prod"
+  github_organization    = "prabhat1800"
+  wif_pool_id            = "github-pool"
+  wif_provider_id        = "github-provider"
+  artifact_repository_id = "gcr-repo"
+}
