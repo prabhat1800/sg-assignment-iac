8d296f9c25364b4ea2bc24bf2173d527d0d1a0de
prabhat <Rupali@com>
2026-08-18 21:57:27 +0530

    correcting name for AR

--- infra/envs/dev/artifact.tf
+++ infra/envs/dev/artifact.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "app" {
   location      = local.region
-  repository_id = local.artifact_repository_id
+  repository_id = "${local.artifact_repository_id}-${local.environment}"
   description   = "Application container images"
   format        = "DOCKER"
 
